ce627122a44f370583f3671654fcec0d948119d2
Swaviman2004 <162682906+Swaviman2004@users.noreply.github.com>
2025-10-15 12:12:00 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://3.110.96.223:8083/back1/auth";
+const API_URL = "http://15.207.236.133/back1/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
